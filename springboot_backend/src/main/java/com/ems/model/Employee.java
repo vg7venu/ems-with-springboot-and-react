c0f785b78493f2c8ff5901538e7cdc9ca3b6dccb
vg7venu <vg7venu@gmail.com>
2021-08-11 15:08:11 +0530

    Adding JP annotations to map model to relational database

--- springboot_backend/src/main/java/com/ems/model/Employee.java
+++ springboot_backend/src/main/java/com/ems/model/Employee.java
@@ -1,9 +1,27 @@
 package com.ems.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "employees")
 public class Employee {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
+	
+	@Column(name = "first_name")
 	private String firstName;
+	
+	@Column(name = "last_name")
 	private String lastName;
+	
+	@Column(name = "email_id")
 	private String emailId;
 	
 	public Employee() {
