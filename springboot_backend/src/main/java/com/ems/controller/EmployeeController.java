c44f9c89e5bd389a3e20ccf1c75d1e77747abd9e
vg7venu <vg7venu@gmail.com>
2021-08-12 18:08:13 +0530

    Springboot: Add delete employee RESTAPI

--- springboot_backend/src/main/java/com/ems/controller/EmployeeController.java
+++ springboot_backend/src/main/java/com/ems/controller/EmployeeController.java
@@ -1,10 +1,13 @@
 package com.ems.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -60,6 +63,19 @@ public class EmployeeController {
 		
 		Employee updatedEmployee = employeeRepository.save(employee);
 		return ResponseEntity.ok(updatedEmployee);
+		
+	}
+	
+	// Delete employee REST API
+	@DeleteMapping("/employees/{id}")
+	public ResponseEntity<Map<String, Boolean>> deleteEmployee(@PathVariable Long id) {
+		Employee employee = employeeRepository.findById(id)
+					.orElseThrow(() -> new ResourceNotFoundException("Employee not exist with id:"  + id));
+		
+		employeeRepository.delete(employee);
+		Map<String, Boolean> response = new HashMap<>();
+		response.put("deleted", Boolean.TRUE);
+		return ResponseEntity.ok(response);
 		
 	}
 }
